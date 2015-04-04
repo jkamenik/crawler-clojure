b047b9d9348e3c92724090b34f11085bb1979e19
John Kamenik <jkamenik@gmail.com>
2015-04-03 20:52:26 -0400

    Adding the basic command struct

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject crawler-clojure "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A simple web crawler"
+  :url "https://github.com/jkamenik/crawler-clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/tools.cli "0.3.1"]]
+  :main ^:skip-aot crawler-clojure.core)
--- src/crawler_clojure/core.clj
+++ src/crawler_clojure/core.clj
@@ -1,6 +1,35 @@
-(ns crawler-clojure.core)
+(ns crawler-clojure.core
+  (:require
+   [clojure.tools.cli :refer [parse-opts]])
+  (:gen-class))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def cli-options
+  [["-h" "--help" "Show help"]
+   ["-d" "--depth NUM" "Max depth" :default 1]])
+
+(defn usage [opts]
+  (println "crawler [options] <URL>")
+  (println "")
+  (println " Crawls a URL for links, then follows those links")
+  (println "")
+  (println "Options:")
+  (println opts)
+  (println "")
+  (println "Example:")
+  (println "  crawler http://somedomain.com")
+  (println "  Home -> /")
+  (println "  About Us -> /about_us.php")
+  (println "  Careers -> http://otherdomain.com/somedomain.com")
+  (println "    Home -> http://somedomain.com")
+  (println "    Careers -> /somedomain.com")
+  (System/exit 1))
+
+(defn -main [& args]
+  ;; (println (parse-opts args cli-options))
+  (let [{:keys [options arguments errors summary]}
+        (parse-opts args cli-options)]
+    (cond
+      (:help options) (usage summary)
+      errors (do (println errors)
+                 (System/exit 1))
+      true (println "Finished"))))
