fa1cc72874f4efd0097a3c30d7c76d32671bf069
John Kamenik <jkamenik@gmail.com>
2015-04-11 23:11:04 -0400

    Rewrite to recurse the links

--- src/crawler_clojure/crawler.clj
+++ src/crawler_clojure/crawler.clj
@@ -16,41 +16,64 @@
   (html/html-resource
    (java.io.StringReader. s)))
 
-(defn abs-link [base link]
+(defn link-from-url [url]
+  {:attrs {:href url}})
+
+(defn url-from-link [link]
   (let [attrs (:attrs link)
         href (:href attrs)]
+    href))
+
+(defn abs-link [base link]
+  (let [href (url-from-link link)]
     (if (not (.startsWith (str href) "http"))
-      (deep-merge link {:attrs {:href (str base href)}})
+      (deep-merge link (link-from-url (str base href)))
       link)))
 
 (defn abs-url [base link]
-  (let [attrs (:attrs link)
-        href (:href attrs)]
-    (println base link attrs href)
-    (cond (.startsWith (str href) "http") href
-          true (str base href))))
+  (let [link (abs-link base link)
+        href (url-from-link link)]
+    href))
 
-(defn print-link [base link depth]
+(defn print-link [link depth]
   (let [indent (apply str (repeat depth "  "))
         body (first (:content link))
-        href (abs-url base link)
+        attrs (:attrs link)
+        href (:href attrs)
         link-str (cond
                    (= :img (:tag body)) (str indent "<image> -> " href)
-                   true (str indent body "->" href))]
+                   true (str indent body " -> " href))]
     (println link-str)))
 
+(defn collect-links [url depth]
+  (let [response (client/get url)
+        body (:body response)
+        elements (parse body)
+        links (find-links elements)]
+    (map (fn [l] (assoc l :depth depth)) links)))
+
 (defn crawl
-  ([url maxdepth] (crawl url maxdepth 0 []))
-  ([url maxdepth current visited]
-   (println "---" url "---" visited)
-   (when (> maxdepth 0)
-     (let [v (cons url visited)
-           response (client/get url)
-           body (:body response)
-           elements (parse body)
-           links (find-links elements)]
-       ;; (println links))))
-       (doseq [link links]
-         (let [])
-         (print-link url link current)
-         (when (not (contains? v ))))))))
+  ([url maxdepth] (crawl {:depth maxdepth :attrs {:href url}} maxdepth 0 [] []))
+  ([link maxdepth current links visited]
+   (let [url (url-from-link link)
+         depth (or (:depth link) 0)]
+     (println "---" depth link maxdepth current visited)
+     ;; (println links)
+     (if (contains? visited url)
+       nil ;; do nothing if visited
+       (when (>= maxdepth depth)
+         (print-link link depth)
+         (let [l (collect-links url current)
+               visited (conj visited url)
+               links (apply conj links l)]
+           (recur (first links)
+                  maxdepth
+                  (inc current)
+                  (vec (rest links))
+                  visited)))))))
+
+;; visit a url
+;; return if already visited
+;; download all links
+;; convert links to abs path
+;; visit each link
